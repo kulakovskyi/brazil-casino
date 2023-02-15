37982fb112b7a320e14c3e6d85363e6386cae5b5
kulakovskyi <torchok9@gmail.com>
2023-02-15 09:31:25 +0200

    test block page3

--- src/js/script-page3.js
+++ src/js/script-page3.js
@@ -1,6 +1,9 @@
 //Реализация бургер-меню
 import { burger } from './functions/burger';
 
+// Dynamic Adaptive
+import { dynamicAdaptive } from './components/dynamicAdaptive.js';
+
 //Footer cookie
 import { footerCookie } from './components/footerCookieComponent';
 
